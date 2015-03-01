3ca5b9f48fa32252352b54cf36c5bb90027d94f4
ChenglongChen <c.chenglong@gmail.com>
2015-03-01 15:38:19 +0800

    add MainCaller.cpp

---
+++ examples/MainCaller.cpp
@@ -0,0 +1,11 @@
+#include "../../tools/caffe.cpp"
+//#include "../../tools/caffe_predict.cpp"
+//#include "../../tools/caffe_predict_average.cpp"
+
+//#include "../../tools/convert_imageset.cpp"
+//#include "../../tools/convert_imageset_compact.cpp"
+//#include "../../tools/compute_image_mean.cpp"
+
+//#include "../../tools/net_speed_benchmark.cpp"
+//#include "../../tools/dump_network.cpp"
+//#include "../../tools/extract_features.cpp"
